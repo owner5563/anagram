19e7a04229e045bd278b34242f2034d363cbdf81
Kyle Parsons and Ryan Putman <ryan@>
2018-06-27 15:01:36 -0700

    initial commit with broken logic

---
+++ Anagrams.Tests/Model.Tests/Anagrams.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagrams;
+using System;
+
+namespace Anagrams.Tests
+{
+    [TestClass]
+    public class WordsTests
+    {
+        [TestMethod]
+        public void SplitWord_GenerateArray_Array()
+        {
+            Words testWords = new Words();
+            CollectionAssert.AreEqual(new char[] { "W", "o", "r", "d" }, testWords.SplitWord("Word"));
+        }
+    }
+}
---
+++ Anagrams/Models/Anagrams.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a first word to compare");
+            string Word1 = Console.ReadLine();
+            Console.WriteLine("Now enter a second word to check if its an anagram of the first ");
+            string Word2 = Console.ReadLine();
+        }
+    }
+    public class Words
+    {
+        public char[] SplitWord(string Word)
+        {
+            char[] Letters = Word.Split(' ');
+            Console.WriteLine(Letters[0]);
+            return Letters;
+        }
+    }
+
+
+}
